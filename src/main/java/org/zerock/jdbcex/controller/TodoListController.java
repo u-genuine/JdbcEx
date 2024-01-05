a6b80a83c6221b8a0e5723c443cd9c522a832faa
u-genuine <u_genuine@naver.com>
2024-01-05 13:57:00 +0900

    ServletContextListener

--- src/main/java/org/zerock/jdbcex/controller/TodoListController.java
+++ src/main/java/org/zerock/jdbcex/controller/TodoListController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.service.TodoService;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,6 +24,10 @@ public class TodoListController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 
         log.info("todo list..................");
+
+        ServletContext servletContext = req.getServletContext();
+
+        log.info("appName: " + servletContext.getAttribute("appName"));
 
         try{
             List<TodoDTO> dtoList = todoService.listAll();
--- src/main/webapp/WEB-INF/todo/list.jsp
+++ src/main/webapp/WEB-INF/todo/list.jsp
@@ -13,6 +13,8 @@
 </head>
 <body>
 <h1>Todo List</h1>
+
+<h2>${appName}</h2>
 <h2>${loginInfo}</h2>
 <h3>${loginInfo.mname}</h3>
 <ul>
